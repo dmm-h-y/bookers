222a75fd4def361c08e666fb28e2cbf9333b8a1a
YukaHayashi <112526288+dmm-h-y@users.noreply.github.com>
2022-08-31 11:29:33 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      #フラッシュメッセージ
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      #newからindexへ変更? @books = Book.all追記
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    #new 貼り付け
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    #フラッシュメッセージ
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      #newからeditへ変更
+      render :edit
+    end
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
